fcc3806307250d79b6ff35980b021f5b685f2584
roni7798 <juventud1@live.com.ar>
2022-01-29 09:00:17 -0300

    Subo proyecto

---
+++ AppGetInfo/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace AppGetInfo
+{
+    partial class FormAppGetInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAppGetInfo));
+            this.lblMotherBoard = new System.Windows.Forms.Label();
+            this.lblGrafica = new System.Windows.Forms.Label();
+            this.lblRAM = new System.Windows.Forms.Label();
+            this.lblAlmacenamiento = new System.Windows.Forms.Label();
+            this.txtMotherBoard = new System.Windows.Forms.TextBox();
+            this.txtTarjetaGrafica = new System.Windows.Forms.TextBox();
+            this.txtRAM = new System.Windows.Forms.TextBox();
+            this.txtAlmacenamiento = new System.Windows.Forms.TextBox();
+            this.lblCantidadRAM = new System.Windows.Forms.Label();
+            this.txtCantidadRAM = new System.Windows.Forms.TextBox();
+            this.btnObtenerInformacion = new System.Windows.Forms.Button();
+            this.lblInfoMother = new System.Windows.Forms.Label();
+            this.lblInfoGrafica = new System.Windows.Forms.Label();
+            this.lblProcesador = new System.Windows.Forms.Label();
+            this.txtProcesador = new System.Windows.Forms.TextBox();
+            this.lblSO = new System.Windows.Forms.Label();
+            this.txtSO = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantSlots = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMotherBoard
+            // 
+            this.lblMotherBoard.Location = new System.Drawing.Point(61, 36);
+            this.lblMotherBoard.Name = "lblMotherBoard";
+            this.lblMotherBoard.Size = new System.Drawing.Size(100, 23);
+            this.lblMotherBoard.TabIndex = 0;
+            this.lblMotherBoard.Text = "MotherBoard";
+            this.lblMotherBoard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGrafica
+            // 
+            this.lblGrafica.Location = new System.Drawing.Point(61, 81);
+            this.lblGrafica.Name = "lblGrafica";
+            this.lblGrafica.Size = new System.Drawing.Size(100, 23);
+            this.lblGrafica.TabIndex = 1;
+            this.lblGrafica.Text = "Tarjeta Gráfica";
+            this.lblGrafica.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRAM
+            // 
+            this.lblRAM.Location = new System.Drawing.Point(61, 128);
+            this.lblRAM.Name = "lblRAM";
+            this.lblRAM.Size = new System.Drawing.Size(100, 23);
+            this.lblRAM.TabIndex = 2;
+            this.lblRAM.Text = "Memoria RAM";
+            this.lblRAM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAlmacenamiento
+            // 
+            this.lblAlmacenamiento.Location = new System.Drawing.Point(61, 217);
+            this.lblAlmacenamiento.Name = "lblAlmacenamiento";
+            this.lblAlmacenamiento.Size = new System.Drawing.Size(100, 23);
+            this.lblAlmacenamiento.TabIndex = 3;
+            this.lblAlmacenamiento.Text = "Almacenamiento";
+            this.lblAlmacenamiento.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMotherBoard
+            // 
+            this.txtMotherBoard.Location = new System.Drawing.Point(178, 38);
+            this.txtMotherBoard.Multiline = true;
+            this.txtMotherBoard.Name = "txtMotherBoard";
+            this.txtMotherBoard.ReadOnly = true;
+            this.txtMotherBoard.Size = new System.Drawing.Size(412, 20);
+            this.txtMotherBoard.TabIndex = 4;
+            // 
+            // txtTarjetaGrafica
+            // 
+            this.txtTarjetaGrafica.Location = new System.Drawing.Point(178, 83);
+            this.txtTarjetaGrafica.Multiline = true;
+            this.txtTarjetaGrafica.Name = "txtTarjetaGrafica";
+            this.txtTarjetaGrafica.ReadOnly = true;
+            this.txtTarjetaGrafica.Size = new System.Drawing.Size(412, 20);
+            this.txtTarjetaGrafica.TabIndex = 5;
+            // 
+            // txtRAM
+            // 
+            this.txtRAM.Location = new System.Drawing.Point(178, 131);
+            this.txtRAM.Multiline = true;
+            this.txtRAM.Name = "txtRAM";
+            this.txtRAM.ReadOnly = true;
+            this.txtRAM.Size = new System.Drawing.Size(150, 20);
+            this.txtRAM.TabIndex = 6;
+            // 
+            // txtAlmacenamiento
+            // 
+            this.txtAlmacenamiento.Location = new System.Drawing.Point(178, 219);
+            this.txtAlmacenamiento.Multiline = true;
+            this.txtAlmacenamiento.Name = "txtAlmacenamiento";
+            this.txtAlmacenamiento.ReadOnly = true;
+            this.txtAlmacenamiento.Size = new System.Drawing.Size(412, 92);
+            this.txtAlmacenamiento.TabIndex = 7;
+            // 
+            // lblCantidadRAM
+            // 
+            this.lblCantidadRAM.Location = new System.Drawing.Point(61, 172);
+            this.lblCantidadRAM.Name = "lblCantidadRAM";
+            this.lblCantidadRAM.Size = new System.Drawing.Size(100, 23);
+            this.lblCantidadRAM.TabIndex = 8;
+            this.lblCantidadRAM.Text = "Cantidad RAM";
+            this.lblCantidadRAM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCantidadRAM
+            // 
+            this.txtCantidadRAM.Location = new System.Drawing.Point(178, 174);
+            this.txtCantidadRAM.Multiline = true;
+            this.txtCantidadRAM.Name = "txtCantidadRAM";
+            this.txtCantidadRAM.ReadOnly = true;
+            this.txtCantidadRAM.Size = new System.Drawing.Size(150, 20);
+            this.txtCantidadRAM.TabIndex = 9;
+            // 
+            // btnObtenerInformacion
+            // 
+            this.btnObtenerInformacion.Location = new System.Drawing.Point(465, 394);
+            this.btnObtenerInformacion.Name = "btnObtenerInformacion";
+            this.btnObtenerInformacion.Size = new System.Drawing.Size(125, 23);
+            this.btnObtenerInformacion.TabIndex = 10;
+            this.btnObtenerInformacion.Text = "Obtener Información";
+            this.btnObtenerInformacion.UseVisualStyleBackColor = true;
+            this.btnObtenerInformacion.Click += new System.EventHandler(this.btnObtenerInformacion_Click);
+            // 
+            // lblInfoMother
+            // 
+            this.lblInfoMother.Location = new System.Drawing.Point(175, 12);
+            this.lblInfoMother.Name = "lblInfoMother";
+            this.lblInfoMother.Size = new System.Drawing.Size(415, 23);
+            this.lblInfoMother.TabIndex = 11;
+            this.lblInfoMother.Text = "Numero de Serie | Fabricado por | Modelo";
+            this.lblInfoMother.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfoGrafica
+            // 
+            this.lblInfoGrafica.Location = new System.Drawing.Point(175, 61);
+            this.lblInfoGrafica.Name = "lblInfoGrafica";
+            this.lblInfoGrafica.Size = new System.Drawing.Size(415, 19);
+            this.lblInfoGrafica.TabIndex = 12;
+            this.lblInfoGrafica.Text = "Driver version | Nombre | Video Processor";
+            this.lblInfoGrafica.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblProcesador
+            // 
+            this.lblProcesador.Location = new System.Drawing.Point(61, 323);
+            this.lblProcesador.Name = "lblProcesador";
+            this.lblProcesador.Size = new System.Drawing.Size(100, 23);
+            this.lblProcesador.TabIndex = 13;
+            this.lblProcesador.Text = "Procesador";
+            this.lblProcesador.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtProcesador
+            // 
+            this.txtProcesador.Location = new System.Drawing.Point(178, 323);
+            this.txtProcesador.Multiline = true;
+            this.txtProcesador.Name = "txtProcesador";
+            this.txtProcesador.ReadOnly = true;
+            this.txtProcesador.Size = new System.Drawing.Size(412, 20);
+            this.txtProcesador.TabIndex = 14;
+            // 
+            // lblSO
+            // 
+            this.lblSO.Location = new System.Drawing.Point(61, 355);
+            this.lblSO.Name = "lblSO";
+            this.lblSO.Size = new System.Drawing.Size(100, 23);
+            this.lblSO.TabIndex = 15;
+            this.lblSO.Text = "Sistema Operativo";
+            this.lblSO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSO
+            // 
+            this.txtSO.Location = new System.Drawing.Point(178, 355);
+            this.txtSO.Multiline = true;
+            this.txtSO.Name = "txtSO";
+            this.txtSO.ReadOnly = true;
+            this.txtSO.Size = new System.Drawing.Size(412, 20);
+            this.txtSO.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(347, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Cantidad Slots";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCantSlots
+            // 
+            this.txtCantSlots.Location = new System.Drawing.Point(440, 177);
+            this.txtCantSlots.Multiline = true;
+            this.txtCantSlots.Name = "txtCantSlots";
+            this.txtCantSlots.ReadOnly = true;
+            this.txtCantSlots.Size = new System.Drawing.Size(150, 20);
+            this.txtCantSlots.TabIndex = 18;
+            // 
+            // FormAppGetInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 442);
+            this.Controls.Add(this.txtCantSlots);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSO);
+            this.Controls.Add(this.lblSO);
+            this.Controls.Add(this.txtProcesador);
+            this.Controls.Add(this.lblProcesador);
+            this.Controls.Add(this.lblInfoGrafica);
+            this.Controls.Add(this.lblInfoMother);
+            this.Controls.Add(this.btnObtenerInformacion);
+            this.Controls.Add(this.txtCantidadRAM);
+            this.Controls.Add(this.lblCantidadRAM);
+            this.Controls.Add(this.txtAlmacenamiento);
+            this.Controls.Add(this.txtRAM);
+            this.Controls.Add(this.txtTarjetaGrafica);
+            this.Controls.Add(this.txtMotherBoard);
+            this.Controls.Add(this.lblAlmacenamiento);
+            this.Controls.Add(this.lblRAM);
+            this.Controls.Add(this.lblGrafica);
+            this.Controls.Add(this.lblMotherBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormAppGetInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AppGetInfo - Roni";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMotherBoard;
+        private System.Windows.Forms.Label lblGrafica;
+        private System.Windows.Forms.Label lblRAM;
+        private System.Windows.Forms.Label lblAlmacenamiento;
+        private System.Windows.Forms.TextBox txtMotherBoard;
+        private System.Windows.Forms.TextBox txtTarjetaGrafica;
+        private System.Windows.Forms.TextBox txtRAM;
+        private System.Windows.Forms.TextBox txtAlmacenamiento;
+        private System.Windows.Forms.Label lblCantidadRAM;
+        private System.Windows.Forms.TextBox txtCantidadRAM;
+        private System.Windows.Forms.Button btnObtenerInformacion;
+        private System.Windows.Forms.Label lblInfoMother;
+        private System.Windows.Forms.Label lblInfoGrafica;
+        private System.Windows.Forms.Label lblProcesador;
+        private System.Windows.Forms.TextBox txtProcesador;
+        private System.Windows.Forms.Label lblSO;
+        private System.Windows.Forms.TextBox txtSO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantSlots;
+    }
+}
+
---
+++ AppGetInfo/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+
+namespace AppGetInfo
+{
+    public partial class FormAppGetInfo : Form
+    {        
+        public FormAppGetInfo()
+        {
+            InitializeComponent();
+            txtAlmacenamiento.ScrollBars = ScrollBars.Both;
+            txtAlmacenamiento.WordWrap = false;
+        }
+
+        private void btnObtenerInformacion_Click(object sender, EventArgs e)
+        {
+            txtMotherBoard.Text = ObtenerInfoMotherboard();
+            txtTarjetaGrafica.Text = ObtenerInformacionGrafica();
+            txtRAM.Text = ObtenerInformacionTipoRAM();
+            txtCantidadRAM.Text = ObtenerInformacionCantidadRAM();
+            txtCantSlots.Text = ObtenerInformacionCantidadSlots();
+            txtAlmacenamiento.Text = ObtenerInformacionDisco();
+            txtProcesador.Text = ObtenerInformacionProcesador();
+            txtSO.Text = ObtenerInformacionSO();
+        }
+
+        public string ObtenerInfoMotherboard()
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_BaseBoard");
+            string motherboard = "";
+            foreach (ManagementObject mo in mos.Get())
+            {
+                try
+                {
+                    motherboard = mo.GetPropertyValue("SerialNumber").ToString()
+                + " - " + mo.GetPropertyValue("Manufacturer").ToString()
+                + " - " + mo.GetPropertyValue("Product").ToString();
+                }
+                catch
+                { }
+            }
+            return motherboard;
+        }
+
+        public string ObtenerInformacionGrafica()
+        {
+            string grafica = "";
+            ManagementObjectSearcher myVideoObject = new ManagementObjectSearcher("select * from Win32_VideoController");
+
+            foreach (ManagementObject obj in myVideoObject.Get())
+            {
+                grafica = obj["DriverVersion"] + " - " + obj["Name"] + " - " + obj["VideoProcessor"];
+            }
+            return grafica;
+        }
+
+        public static string FormatBytes(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+
+        public string ObtenerInformacionTipoRAM()
+        {
+                int type = 0;
+
+                ConnectionOptions connection = new ConnectionOptions();
+                connection.Impersonation = ImpersonationLevel.Impersonate;
+                ManagementScope scope = new ManagementScope("\\\\.\\root\\CIMV2", connection);
+                scope.Connect();
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    type = Convert.ToInt32(queryObj["MemoryType"]);
+                }
+
+                return TypeString(type);
+        }
+
+        private static string TypeString(int type)
+        {
+            string outValue = string.Empty;
+
+            switch (type)
+            {
+                case 0x0: outValue = "Unknown"; break;
+                case 0x1: outValue = "Other"; break;
+                case 0x2: outValue = "DRAM"; break;
+                case 0x3: outValue = "Synchronous DRAM"; break;
+                case 0x4: outValue = "Cache DRAM"; break;
+                case 0x5: outValue = "EDO"; break;
+                case 0x6: outValue = "EDRAM"; break;
+                case 0x7: outValue = "VRAM"; break;
+                case 0x8: outValue = "SRAM"; break;
+                case 0x9: outValue = "RAM"; break;
+                case 0xa: outValue = "ROM"; break;
+                case 0xb: outValue = "Flash"; break;
+                case 0xc: outValue = "EEPROM"; break;
+                case 0xd: outValue = "FEPROM"; break;
+                case 0xe: outValue = "EPROM"; break;
+                case 0xf: outValue = "CDRAM"; break;
+                case 0x10: outValue = "3DRAM"; break;
+                case 0x11: outValue = "SDRAM"; break;
+                case 0x12: outValue = "SGRAM"; break;
+                case 0x13: outValue = "RDRAM"; break;
+                case 0x14: outValue = "DDR"; break;
+                case 0x15: outValue = "DDR2"; break;
+                case 0x16: outValue = "DDR2 FB-DIMM"; break;
+                case 0x17: outValue = "Undefined 23"; break;
+                case 0x18: outValue = "DDR3"; break;
+                case 0x19: outValue = "FBD2"; break;
+                case 0x1a: outValue = "DDR4"; break;
+                default: outValue = "Undefined"; break;
+            }
+
+            return outValue;
+        }
+        public string ObtenerInformacionCantidadRAM()
+        {
+            string cantidadRAM = "";
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                long cantidadMB = long.Parse(result["TotalVisibleMemorySize"].ToString());
+                string cantidadGB = FormatBytes(cantidadMB * 1024);
+                cantidadRAM = cantidadGB + " (Disponible)";            
+            }
+            return cantidadRAM;
+        }
+
+        public string ObtenerInformacionCantidadSlots()
+        {
+            ManagementObjectSearcher searcher;
+            string cantidadSlots = "";
+            try
+            {
+                searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PhysicalMemoryArray");
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    cantidadSlots = queryObj["MemoryDevices"].ToString();
+                }
+            }
+            catch (ManagementException e)
+            {
+                System.IO.File.AppendAllText(@"ErrorAppAD.txt", "Opps... Tuvimos el siguiente error en consultaRAM: " + e.ToString());
+            }            
+            return cantidadSlots;
+        }
+
+        public string ObtenerInformacionDisco()
+        {
+            string disco = "";
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                disco += "------------------------------";
+                disco += "\r\nDisco: " + d.Name;
+                if (d.IsReady == true)
+                {
+                    disco += "\r\nFormato: " + d.DriveFormat;
+                    disco += "\r\nEspacio Disponible: " + FormatBytes(d.TotalFreeSpace);
+                    disco += "\r\nEspacio Utilizado: " + FormatBytes(d.TotalSize - d.TotalFreeSpace);
+                    disco += "\r\nEspacio Total: " + FormatBytes(d.TotalSize);
+                    disco += "\r\n------------------------------";
+                }
+            }
+            return disco;
+        }
+
+        public string ObtenerInformacionProcesador()
+        {
+            string procesador = "";
+            ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+
+            foreach (ManagementObject obj in myProcessorObject.Get())
+            {
+                procesador += obj["Name"];
+            }
+            return procesador;
+        }
+
+        public string ObtenerInformacionSO()
+        {
+            ManagementObjectSearcher myOperativeSystemObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+            string so = "";
+            foreach (ManagementObject obj in myOperativeSystemObject.Get())
+            {
+                so +=  obj["Caption"];
+               
+            }
+            return so;
+        }
+
+
+    }
+}
